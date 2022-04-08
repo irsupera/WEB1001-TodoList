bc5528fd4ee5fff2f17d1eac22c6679342056e6c
irsupera <97858090+irsupera@users.noreply.github.com>
2022-04-08 19:44:25 -0400

    Create TodoList and add Model
    Create TodoList and add Model

---
+++ TodoList/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Models
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime AddDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CompleteDate { get; set; }
+    }
+}
